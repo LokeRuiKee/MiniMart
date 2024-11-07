a53cb3d4c11b135ba9894db00e87d183a618d0b9
ptplokee <lokerui.kee@flex.com>
2024-11-07 09:09:11 +0800

    add annotation and augmentation script

--- datasetPreparation/augmentation.py
+++ datasetPreparation/augmentation.py
@@ -13,32 +13,29 @@ transform = A.Compose([
     A.CLAHE(),
     A.RandomRotate90(),
     A.Transpose(),
-    A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.50,
-                       rotate_limit=45, p=0.75),
     A.Blur(blur_limit=3),
-    A.OpticalDistortion(),
-    A.GridDistortion(),
-    A.HueSaturationValue()
+    A.HueSaturationValue(),
+    A.Affine(scale=(0.9, 0.7), rotate=(-15,15), shear=(-25,25), keep_ratio=True, balanced_scale=True)
 ])
 
-# Iterate over each image in subdirectories
-for image_path in input_folder.rglob('*.[pj][np]g'):  # Adjust pattern as needed
-    # Read the image
-    image = cv2.imread(str(image_path))
-    if image is None:
-        print(f"Warning: Couldn't open {image_path}. Skipping...")
-        continue
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+## Iterate over each image in subdirectories
+#for image_path in input_folder.rglob('*.[pj][np]g'):  # Adjust pattern as needed
+#    # Read the image
+#    image = cv2.imread(str(image_path))
+#    if image is None:
+#        print(f"Warning: Couldn't open {image_path}. Skipping...")
+#        continue
+#    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # Apply augmentations
-    augmented_image = transform(image=image)['image']
+#    # Apply augmentations
+#    augmented_image = transform(image=image)['image']
 
-    # Determine output path
-    relative_path = image_path.relative_to(input_folder)
-    output_image_path = output_folder / relative_path.parent / f"processed_{image_path.stem}.jpg"
-    output_image_path.parent.mkdir(parents=True, exist_ok=True)  # Create subdirectories if needed
+#    # Determine output path
+#    relative_path = image_path.relative_to(input_folder)
+#    output_image_path = output_folder / relative_path.parent / f"processed_{image_path.stem}.jpg"
+#    output_image_path.parent.mkdir(parents=True, exist_ok=True)  # Create subdirectories if needed
 
-    # Save the augmented image
-    cv2.imwrite(str(output_image_path), cv2.cvtColor(augmented_image, cv2.COLOR_RGB2BGR))
+#    # Save the augmented image
+#    cv2.imwrite(str(output_image_path), cv2.cvtColor(augmented_image, cv2.COLOR_RGB2BGR))
 
-    print(f"Saved processed image to {output_image_path}")
+#    print(f"Saved processed image to {output_image_path}")
---
+++ datasetPreparation/config_temp.py
@@ -0,0 +1,11 @@
+# Paths
+MODEL_PATH = "../model/martModelv4/weights/best.pt"
+JSON_DIRECTORY = "C:\\Users\\ptplokee\\Source\\Repos\\MiniMart\\prototypev2\\static"
+JSON_PATH = "C:\\Users\\ptplokee\\Source\\Repos\\MiniMart\\prototypev2\\static\\flask_detect.json"
+JSON_FILE_NAME = "flask_detect.json"
+
+# YOLO Model Settings
+CONFIDENCE_THRESHOLD = 0.7
+
+# Logging Settings
+LOGGING_INTERVAL = 5  # seconds
