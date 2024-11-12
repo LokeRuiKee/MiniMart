8c52dfcf17f50ba7c4c7a7b9295dd5673b554190
ptplokee <lokerui.kee@flex.com>
2024-11-12 09:38:51 +0800

    initial commit for admin add record

--- datasetPreparation/data_config.py
+++ datasetPreparation/data_config.py
@@ -1,11 +1,11 @@
 # Paths
-INPUT_DATA_DIRECTORY = "C:\\Users\\ptplokee\\source\\pipelineDatasetv1\\raw"
-OUTPUT_DATA_DIRECTORY = "C:\\Users\\ptplokee\\source\\pipelineDatasetv1\\processed"
+INPUT_DATA_DIRECTORY = "C:\\Users\\ptplokee\\source\\pipelineDatasetv2\\raw"
+OUTPUT_DATA_DIRECTORY = "C:\\Users\\ptplokee\\source\\pipelineDatasetv2\\processed"
 
-PREPROCESSED_OUTPUT_DIRECTORY = "C:\\Users\\ptplokee\\source\\pipelineDatasetv1\\preprocessed"
-AUGMENTED_PREPRO_OUTPUT_DIRECTORY = "C:\\Users\\ptplokee\\source\\pipelineDatasetv1\\augmented_preprocessed"
-AUGMENTED_RAW_OUTPUT_DIRECTORY = "C:\\Users\ptplokee\\source\\pipelineDatasetv1\\augmented_raw"
+PREPROCESSED_OUTPUT_DIRECTORY = "C:\\Users\\ptplokee\\source\\pipelineDatasetv2\\preprocessed"
+AUGMENTED_PREPRO_OUTPUT_DIRECTORY = "C:\\Users\\ptplokee\\source\\pipelineDatasetv2\\augmented_preprocessed"
+AUGMENTED_RAW_OUTPUT_DIRECTORY = "C:\\Users\ptplokee\\source\\pipelineDatasetv2\\augmented_raw"
 
 # Annotations
-INPUT_ANNOTATION_DATA_DIRECTORY = "C:\\Users\\ptplokee\\source\\pipelineDatasetv1\\annotation\\images"
-OUTPUT_ANNOTATION_DATA_DIRECTORY = "C:\\Users\\ptplokee\\source\\pipelineDatasetv1\\annotation\\labels"
+INPUT_ANNOTATION_DATA_DIRECTORY = "C:\\Users\\ptplokee\\source\\pipelineDatasetv2\\annotation\\images"
+OUTPUT_ANNOTATION_DATA_DIRECTORY = "C:\\Users\\ptplokee\\source\\pipelineDatasetv2\\annotation\\labels"
